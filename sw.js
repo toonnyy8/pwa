9f99e7b8baeebd94480911609a8ba45ea657b9cf
toonnyy8 <toonnyy8@gmail.com>
2019-10-02 22:29:37 +0800

    update

--- sw.js
+++ sw.js
@@ -1,18 +1,34 @@
 console.log('Hello from sw.js')
 
-let CACHE_NAME =
+let CACHE_NAME = "cacheV1"
+var urlsToCache = [
+    '/',
+    './src/js/main.js'
+];
 
-    self.addEventListener("install", (e) => {
-        console.log(e)
-        event.waitUntil(
-            caches.open(CACHE_NAME)
-                .then(function (cache) {
-                    console.log('Opened cache');
-                    return cache.addAll(urlsToCache);
-                })
-        );
-    })
+
+self.addEventListener("install", (e) => {
+    console.log(e)
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+})
 
 self.addEventListener('fetch', (e) => {
     console.log(e)
+    e.respondWith(
+        caches.match(e.request)
+            .then((response) => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(e.request);
+            }
+            )
+    );
 });
